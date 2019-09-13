bc6b9cf533841a6f91d19f8cedba0a7dfc199779
CSMS <CSMS@192.168.9.120>
2019-09-13 11:29:25 -0500

    INitial Commit

---
+++ src/ArraysPractice/DuplicateArray.java
@@ -0,0 +1,27 @@
+package ArraysPractice;
+
+public class DuplicateArray {
+
+	public static void main(String[] args) {
+//		String queries [] ={ "1","2","3","4","5","6","1"};
+//		
+//		
+//		for (int j=0; j<queries.length; j++){
+//			
+//			for (int k=j+1; j<queries.length; k++){
+//				
+//				if (queries[j].equals(queries[k])) {
+//					System.out.println("dupliacte array: "+ queries[j]);
+//				}
+//			}
+//		}
+		
+	
+		
+		String reverse="Amazon";
+		
+		for (int i = reverse.length()-1; i >=0; i--) {
+			System.out.print("  " + reverse.charAt(i));
+			
+		}
+}}
